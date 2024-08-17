56775837922e35b11be422333930447332bda87d
J0R6IT0 <jorgeparpar@gmail.com>
2024-08-17 23:26:28 +0200

    refactor(packages/ui): ignore disable ts check for TextInput

--- packages/ui/src/components/TextField.tsx
+++ packages/ui/src/components/TextField.tsx
@@ -1,3 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
 import { TextField as KTextField } from "@kobalte/core/text-field";
 import { X } from "lucide-solid";
 import type { Component } from "solid-js";
