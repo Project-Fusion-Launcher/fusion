fa9823cf31b7d82c6fa6088865d6daa47b4e0da0
Jorge Pardo <jorgeparpar@gmail.com>
2024-10-05 13:39:42 +0200

    feat(apps/desktop): make game cards buttons to improve accessibility

--- apps/desktop/src/components/GameCard.tsx
+++ apps/desktop/src/components/GameCard.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@kobalte/core/button";
 import { ArrowDownToLine } from "lucide-solid";
 
 interface GameCardProps {
@@ -8,7 +9,7 @@ interface GameCardProps {
 
 const GameCard = (props: GameCardProps) => {
   return (
-    <div
+    <Button
       class="w-192 group flex flex-shrink-0 flex-col hover:cursor-pointer"
       onClick={() => props.onClick && props.onClick()}
     >
@@ -21,13 +22,13 @@ const GameCard = (props: GameCardProps) => {
           <ArrowDownToLine class="stroke-primary size-1/3" />
         </div>
       </div>
-      <span class="text-primary mb-8 mt-16 w-full overflow-hidden text-ellipsis text-nowrap font-medium">
+      <span class="text-primary mb-8 mt-16 w-full overflow-hidden text-ellipsis text-nowrap text-left text-base font-medium">
         {props.title}
       </span>
-      <span class="text-secondary w-full overflow-hidden text-ellipsis text-nowrap text-[14px] font-medium leading-[14px]">
+      <span class="text-secondary w-full overflow-hidden text-ellipsis text-nowrap text-left text-[14px] font-medium leading-[14px]">
         {props.developer}
       </span>
-    </div>
+    </Button>
   );
 };
 
