0fdfc9cd79a67bcf3e236640e193c33f5a50c68a
J0R6IT0 <jorgeparpar@gmail.com>
2025-02-05 20:09:38 +0100

    refactor(packages/ui): use tailwind merge for classes

--- packages/ui/src/utils.ts
+++ packages/ui/src/utils.ts
@@ -1,7 +1,7 @@
 import type { ClassValue } from "tailwind-variants";
 import { clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  // This should ideally include tailwind-merge
-  return clsx(inputs);
+  return twMerge(clsx(inputs));
 }
