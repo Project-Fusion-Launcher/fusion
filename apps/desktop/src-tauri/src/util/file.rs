422341f54e3d1b6e486930f0d948157e194e1d2a
Jorge Pardo <jorgeparpar@gmail.com>
2025-02-06 10:39:50 +0100

    refactor(apps/desktop): allow compiling on linux

--- apps/desktop/src-tauri/src/util/file.rs
+++ apps/desktop/src-tauri/src/util/file.rs
@@ -2,7 +2,8 @@ use crate::common::result::Result;
 use std::path::Path;
 use tokio::fs;
 
-const NO_WINDOW_FLAGS: u32 = 0x08000000;
+#[cfg(target_os = "windows")]
+const NO_WINDOW_FLAG: u32 = 0x08000000;
 
 pub async fn extract_file<P>(file_path: &P, output_dir: &P) -> Result<()>
 where
@@ -22,15 +23,20 @@ where
     let exe_path = std::env::current_exe()?;
     let seven_zip = exe_path.parent().unwrap().join("thirdparty/7-Zip/7z.exe");
 
-    let result = tokio::process::Command::new(seven_zip)
+    let mut command = tokio::process::Command::new(seven_zip);
+    command
         .arg("x")
         .arg(file_path)
         .arg(format!("-o{}", output_dir.to_string_lossy()))
         .arg("-aoa")
-        .arg("-x!$PLUGINSDIR/*")
-        .creation_flags(NO_WINDOW_FLAGS)
-        .output()
-        .await?;
+        .arg("-x!$PLUGINSDIR/*");
+
+    #[cfg(target_os = "windows")]
+    {
+        command.creation_flags(NO_WINDOW_FLAG);
+    }
+
+    let result = command.output().await?;
 
     println!("7z output: {:?}", String::from_utf8_lossy(&result.stdout));
 
@@ -53,11 +59,17 @@ where
         return Err(format!("File does not exist: {:?}", file_path).into());
     }
 
-    let result = tokio::process::Command::new(file_path)
-        .current_dir(file_path.parent().unwrap())
-        .creation_flags(NO_WINDOW_FLAGS)
-        .spawn()
-        .map_err(|e| e.to_string())?;
+    let mut command = tokio::process::Command::new(file_path);
+
+    let parent_dir = file_path.parent().unwrap();
+    command.current_dir(parent_dir);
+
+    #[cfg(target_os = "windows")]
+    {
+        command.creation_flags(NO_WINDOW_FLAG);
+    }
+
+    let result = command.spawn().map_err(|e| e.to_string())?;
 
     println!("{:?}", result);
 
